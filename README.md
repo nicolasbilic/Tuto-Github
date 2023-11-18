cc195aaca1a4ca63905d79b2b3c7bc79d253c44b
nicolasbilic <nicolasbilic9@gmail.com>
2023-11-18 19:51:17 +0100

    First Commit For Tuto Github VSCode

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Faire mon Devoir avec Github
+
+Je teste Github et VSCode
---
+++ src/Entity/Users.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jobs\Steve\Entity;
+
+Class Users {
+    private int $user_id;
+    private string $password;
+    private int $login;
+}
